e82622cefe9414ced17a877ab201910b65fca8cc
Reithan <bo122081@hotmail.com>
2025-03-29 05:13:16 -0700

    Update negative_rejection_steering_script.py

--- scripts/negative_rejection_steering_script.py
+++ scripts/negative_rejection_steering_script.py
@@ -11,9 +11,9 @@ class NRSScript(scripts.Script):
     def __init__(self):
         super().__init__()
         self.enabled = False
-        self.skew = 2.0
-        self.stretch = 2.0
-        self.squash = 1.0
+        self.skew = 1.0
+        self.stretch = 1.0
+        self.squash = 0.0
 
     sorting_priority = 5
 
